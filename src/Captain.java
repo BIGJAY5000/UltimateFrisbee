64057a875fd1c83ce733b1b7775c6cac7d1d5046
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2024-02-26 09:24:05 -0500

    gnfbf

--- src/Captain.java
+++ src/Captain.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class Captain {
+public class Captain extends UltimatePlayer{
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    //Constructors
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    //Mutators
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    //Accessors
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String toString(){
+        return "Name: " + getFirstName() + " " + getLastName();
+    }
 }
