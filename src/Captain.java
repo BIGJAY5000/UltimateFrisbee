1018d7fe685c4a9eb43c35737fe81af9e620205e
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2024-02-27 23:16:41 -0500

    Update Captain.java

--- src/Captain.java
+++ src/Captain.java
@@ -1,3 +1,37 @@
 public class Captain extends UltimatePlayer{
+    //variables
+    public boolean type;
 
+    //Constructor
+    public Captain(String firstName, String lastName, String position, int jerseyNumber, boolean type){
+        super(firstName, lastName, position, jerseyNumber);
+        this.type = type;
+    }
+
+    //Accessors
+    public boolean getType(){
+        return type;
+    }
+
+    public int throwDisc(int pow){
+        if (pow > 1) {
+            pow = 1;
+        }
+        else if (pow > 10) {
+            pow = 10;
+        }
+        return 5*pow;
+    }
+
+    //Mutators
+    public String toString(){
+        String s = "";
+        s += super.toString() + "\n   Jersey #: " + getJerseyNumber() + "\n   Position: " + getPosition();
+        if (getType()) {
+            s += "\n   Captain: Offense";
+        } else {
+            s += "\n   Captain: Defense";
+        }
+        return s;
+    }
 }
