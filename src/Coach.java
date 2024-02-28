18b83516bc31417df113028f0f02b909b9e1a5d3
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2024-02-27 23:16:59 -0500

    Update Coach.java

--- src/Coach.java
+++ src/Coach.java
@@ -1,2 +1,22 @@
 public class Coach extends Person{
+    public String role;
+
+    public Coach(String firstName, String lastName, String role) {
+        super(firstName, lastName);
+        setRole(role);
+    }
+
+    public void setRole(String role){
+        this.role = role;
+    }
+
+    public String getRole(){
+        return role;
+    }
+
+    public String toString(){
+        String s = "";
+        s += super.toString() + "\n   Role: " + getRole();
+        return s;
+    }
 }
